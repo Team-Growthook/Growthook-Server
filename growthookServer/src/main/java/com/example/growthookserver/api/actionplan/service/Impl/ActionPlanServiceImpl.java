12e4512e8913d2224094d53865974c4771a78bc7
Hong0329 <rhkgkrwkhj@naver.com>
2023-12-20 00:50:51 +0900

    [FEAT]액션 플랜 조회 API 개발

--- growthookServer/src/main/java/com/example/growthookserver/api/actionplan/service/Impl/ActionPlanServiceImpl.java
+++ growthookServer/src/main/java/com/example/growthookserver/api/actionplan/service/Impl/ActionPlanServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.growthookserver.api.actionplan.service.Impl;
 import com.example.growthookserver.api.actionplan.domain.ActionPlan;
 import com.example.growthookserver.api.actionplan.dto.request.ActionPlanCreateRequestDto;
 import com.example.growthookserver.api.actionplan.dto.response.ActionPlanCreateResponseDto;
+import com.example.growthookserver.api.actionplan.dto.response.ActionPlanGetResponseDto;
 import com.example.growthookserver.api.actionplan.repository.ActionPlanRepository;
 import com.example.growthookserver.api.actionplan.service.ActionPlanService;
 import com.example.growthookserver.api.seed.domain.Seed;
@@ -10,6 +11,9 @@ import com.example.growthookserver.api.seed.repository.SeedRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +33,14 @@ public class ActionPlanServiceImpl implements ActionPlanService {
                 .build();
         ActionPlan savedActionPlan = actionPlanRepository.save(actionPlan);
         return ActionPlanCreateResponseDto.of(savedActionPlan.getId());
+    }
+
+    @Override
+    public List<ActionPlanGetResponseDto> getActionPlan(Long seedId) {
+        List<ActionPlan> actionPlans = actionPlanRepository.findAllBySeedId(seedId);
+
+        return actionPlans.stream()
+                .map(actionPlan -> ActionPlanGetResponseDto.of(actionPlan.getId(), actionPlan.getContent(), actionPlan.getIsScraped(), actionPlan.getIsFinished()))
+                .collect(Collectors.toList());
     }
 }
